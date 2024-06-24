42c9337a52a5a406c08c91e54a0cbcbe9325d52f
“nicm42” <42087659+nicm42@users.noreply.github.com>
2024-06-24 15:26:23 +0100

    Rearranged the section contents

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import '/scss/styles.scss';
 const introSection = document.querySelector('.intro');
 const theAnswerSection = document.querySelector('.the-answer');
 const earthSection = document.querySelector('.earth');
+const earthExplodedSection = document.querySelector('.earth-exploded');
 const theQuestionSection = document.querySelector('.the-question');
 
 const showTheAnswerButton = document.querySelector('.show-the-answer');
@@ -18,9 +19,15 @@ showTheAnswerButton.addEventListener('click', () => {
 showEarthButton.addEventListener('click', () => {
   theAnswerSection.classList.remove('show');
   earthSection.classList.add('show');
+
+  // After the Earth has exploded, show the next section
+  setTimeout(() => {
+    earthSection.classList.remove('show');
+    earthExplodedSection.classList.add('show');
+  }, 2000);
 });
 
 showTheQuestionButton.addEventListener('click', () => {
-  earthSection.classList.remove('show');
+  earthExplodedSection.classList.remove('show');
   theQuestionSection.classList.add('show');
 });
